f98d5f2799dc21ad2cdf2625944683fb96aeb752
Christoffer Niska <ChristofferNiska@gmail.com>
2013-06-27 23:53:44 +0300

    Add an empty widget behavior

---
+++ behaviors/WidgetBehavior.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * WidgetBehavior class file.
+ * @author Christoffer Niska <christoffer.niska@gmail.com>
+ * @copyright Copyright &copy; Christoffer Niska 2013-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @package crisu83.yii-extension.behaviors
+ */
+
+namespace crisu83\yii_extension\behaviors;
+
+/**
+ * Extension behavior for widgets.
+ */
+class WidgetBehavior extends ComponentBehavior
+{
+}
